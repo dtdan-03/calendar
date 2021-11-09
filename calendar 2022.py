5c18f195a93b7bbf842f1ec315383860b323d464
dtdan03 <93221652+dtdan-03@users.noreply.github.com>
2021-11-09 18:51:13 +0100

    Add files via upload

---
+++ calendar 2022.py
@@ -0,0 +1,51 @@
+import calendar
+
+# calendar  for 2022
+
+year = 2022
+month =1
+print(calendar.month(year,month))
+
+year = 2022
+month =2
+print(calendar.month(year,month))
+
+year = 2022
+month =3
+print(calendar.month(year,month))
+
+year = 2022
+month =4
+print(calendar.month(year,month))
+
+year = 2022
+month =5
+print(calendar.month(year,month))
+
+year = 2022
+month =6
+print(calendar.month(year,month))
+
+year = 2022
+month =7
+print(calendar.month(year,month))
+
+year = 2022
+month =8
+print(calendar.month(year,month))
+
+year = 2022
+month =9
+print(calendar.month(year,month))
+
+year = 2022
+month =10
+print(calendar.month(year,month))
+
+year = 2022
+month =11
+print(calendar.month(year,month))
+
+year = 2022
+month =12
+print(calendar.month(year,month))
