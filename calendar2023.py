8d403628c02891eedd16a12c97335cf9d42c8590
Daniel Tiessen <daniel.tiessen@perfact.de>
2023-10-08 19:36:04 +0200

    added cal for 2023

--- calendar2023.py
+++ calendar2023.py
@@ -1,1 +1,57 @@
 #2023
+
+import calendar
+
+# calendar example for 2023
+
+# set year: year=20xx
+# set month: month=1 /month=2 /month=...     (January: 1; February: 2; ...; December: 12)
+
+year = 2023
+month =1
+print(calendar.month(year,month))
+
+year = 2023
+month =2
+print(calendar.month(year,month))
+
+year = 2023
+month =3
+print(calendar.month(year,month))
+
+year = 2023
+month =4
+print(calendar.month(year,month))
+
+year = 2023
+month =5
+print(calendar.month(year,month))
+
+year = 2023
+month =6
+print(calendar.month(year,month))
+
+year = 2023
+month =7
+print(calendar.month(year,month))
+
+year = 2023
+month =8
+print(calendar.month(year,month))
+
+year = 2023
+month =9
+print(calendar.month(year,month))
+
+year = 2023
+month =10
+print(calendar.month(year,month))
+
+year = 2023
+month =11
+print(calendar.month(year,month))
+
+year = 2023
+month =12
+print(calendar.month(year,month))
+
