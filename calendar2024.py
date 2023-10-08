71616a0b3c1b1132e5b3d139f1caa57414e2f436
Daniel Tiessen <daniel.tiessen@perfact.de>
2023-10-08 20:02:16 +0200

    added cal for 2024

---
+++ calendar2024.py
@@ -0,0 +1,57 @@
+#Calendar for 2024
+
+import calendar
+
+# calendar example for 2024
+
+# set year: year=20xx
+# set month: month=1 /month=2 /month=...     (January: 1; February: 2; ...; December: 12)
+
+year = 2024
+month =1
+print(calendar.month(year,month))
+
+year = 2024
+month =2
+print(calendar.month(year,month))
+
+year = 2024
+month =3
+print(calendar.month(year,month))
+
+year = 2024
+month =4
+print(calendar.month(year,month))
+
+year = 2024
+month =5
+print(calendar.month(year,month))
+
+year = 2024
+month =6
+print(calendar.month(year,month))
+
+year = 2024
+month =7
+print(calendar.month(year,month))
+
+year = 2024
+month =8
+print(calendar.month(year,month))
+
+year = 2024
+month =9
+print(calendar.month(year,month))
+
+year = 2024
+month =10
+print(calendar.month(year,month))
+
+year = 2024
+month =11
+print(calendar.month(year,month))
+
+year = 2024
+month =12
+print(calendar.month(year,month))
+
